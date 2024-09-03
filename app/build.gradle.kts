d9f6cab68b33633b0b5a3e92d257505ac9f4d822
akshay.harale <akshay.harale@gmail.com>
2024-09-03 20:51:57 +0530

    added dagger & retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.dagger)
+    kapt(libs.google.dagger.compiler)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
